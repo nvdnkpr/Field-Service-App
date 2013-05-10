ce69cc83578ad3c8eca28454cd18319f0559fa39
Alan Leard <aleard@appcelerator.com>
2013-05-10 16:47:30 -0700

    Making secret default more descriptive

--- acs/NodeService/app.js
+++ acs/NodeService/app.js
@@ -5,7 +5,7 @@ function start(app, express) {
 	app.use(express.cookieParser());
 	app.use(express.session({
 		key: 'node.acs',
-		secret: '[REPLACE THIS VALUE WITH YOUR OWN]'
+		secret: '[REPLACE THIS WITH YOUR OWN SECRET]'
 	}));
 	app.use(express.favicon(__dirname + '/public/images/favicon.ico')); //set favicon
 }
